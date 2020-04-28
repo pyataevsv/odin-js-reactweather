02e1bae4e3f5d9d622713969226ac5eb27e5f603
pyataevsv <pyataevsv@gmail.com>
2020-04-28 22:20:10 +0300

    Updates

---
+++ precache-manifest.237af3a8f01854ed41402f26ec611bdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c6b91102d6bdca918265975c7d1cfa",
+    "url": "/odin-js-reactweather/index.html"
+  },
+  {
+    "revision": "5b82a6bf10483fbf537b",
+    "url": "/odin-js-reactweather/static/css/main.033fc042.chunk.css"
+  },
+  {
+    "revision": "ae8420f11f8a59ddc2a8",
+    "url": "/odin-js-reactweather/static/js/2.f5cf54e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/odin-js-reactweather/static/js/2.f5cf54e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b82a6bf10483fbf537b",
+    "url": "/odin-js-reactweather/static/js/main.b0e45d30.chunk.js"
+  },
+  {
+    "revision": "7e5055581c95a55d471e",
+    "url": "/odin-js-reactweather/static/js/runtime-main.7c525d8f.js"
+  }
+]);
